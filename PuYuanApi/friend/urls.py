74f84dcdb69defe2aa0c78878d7149270d5b178c
Eterna-E <o23306559@gmail.com>
2020-10-10 17:47:46 +0800

    2020/10/10 backup

--- PuYuanApi/friend/urls.py
+++ PuYuanApi/friend/urls.py
@@ -3,5 +3,8 @@ from .views import *
 
 
 urlpatterns = [
-    path('send/', friend_send),
+    path('send/', friend_send), #可
+    path('code/', friend_code), #可
+    path('list/', friend_list), #可，未合併
+    path('requests/', friend_requests), #可，未合併
 ]
--- PuYuanApi/friend/views.py
+++ PuYuanApi/friend/views.py
@@ -8,7 +8,99 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 
 @csrf_exempt
-def friend_send(request): # 送出控糖團邀請!
+def friend_code(request): # 16.獲取控糖團邀請碼
+    uid = request.user.id
+    uid = 123
+    try:
+        user_friend = Friend.objects.get(uid=uid)
+    except:
+        output = {"status":"1"}
+    else:
+        output = {"status":"0", "invite_code":user_friend.invite_code}
+    return JsonResponse(output,safe=False)
+
+@csrf_exempt
+def friend_list(request): # 17.控糖團列表
+    uid = request.user.id
+    uid = 123
+    if request.method == 'GET':
+        if Friend_data.objects.filter(uid=uid, status=0):
+            friends = []
+            friends_list = Friend_data.objects.filter(uid=uid, status=0)
+            for friend in friends_list:
+                r = {
+                        "id": 2,
+                        "name": "QQ",
+                        "account": "fb_1",
+                        "email": "null",
+                        "phone": "null",
+                        "fb_id": "1",
+                        "status": "Normal",
+                        "group": "null",
+                        "birthday": "1995-10-10",
+                        "height": 171,
+                        "gender": "男",
+                        "verified": 1,
+                        "privacy_policy": 1,
+                        "must_change_password": 0,
+                        "badge": 87,
+                        "created_at": "2017-10-23 14:39:06",
+                        "updated_at": "2017-10-23 19:41:53",
+                        "relation_type": 1
+                }
+                friends.append(r)
+            output = {"status":"0", "friends":friends}
+        else:
+            output = {"status":"1"}
+    return JsonResponse(output)
+
+@csrf_exempt
+def friend_requests(request): # 18.獲取控糖團邀請
+    uid = request.user.id
+    uid = 123
+    try:
+        requests_list = Friend_data.objects.filter(relation_id=uid, status=0)
+    except:
+        output = {"status":"1"}
+    else:
+        requests = []
+        for request in requests_list:
+            created_at_friendata = datetime.strftime(request.created_at, '%Y-%m-%d %H:%M:%S')
+            updated_at_friendata = datetime.strftime(request.updated_at, '%Y-%m-%d %H:%M:%S')
+            r = {
+                "id":request.id,
+                "user_id":request.uid,
+                "relation_id":request.relation_id,
+                "type":request.friend_type,
+                "status":request.status,
+                "created_at":created_at_friendata,
+                "updated_at":updated_at_friendata,
+                "user":{
+                            "id": 2,
+                            "name": "null",
+                            "account": "fb_2",
+                            "email": "null",
+                            "phone": "null",
+                            "fb_id": "2",
+                            "status": "Normal",
+                            "group": "null",
+                            "birthday": "null",
+                            "height": "null",
+                            "gender": "null",
+                            "verified": 1,
+                            "privacy_policy": 1,
+                            "must_change_password": 0,
+                            "badge": 87,
+                            "created_at": "2017-10-20 15:43:47",
+                            "updated_at": "2017-10-20 15:43:54"
+                        }
+                    }
+            requests.append(r)
+        output = {"status":"0", "requests":requests}
+    return JsonResponse(output)
+
+@csrf_exempt
+def friend_send(request): # 19.送出控糖團邀請
     uid = request.user.id
     uid = 123
     if request.method == 'POST':
